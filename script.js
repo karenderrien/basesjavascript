2c780e43e5819937791249f4c9467330e34ace51
Karen Derrien <derrienkaren@gmail.com>
2018-08-28 18:24:26 +0200

    ajout Récupérer les touches frappées par l'utilisateur scriptjs

--- script.js
+++ script.js
@@ -900,3 +900,44 @@ function messagePara(){
     });
 </script>*/
 
+/*<!--Récupérer les touches frappées par l'utilisateur-->
+
+<p>
+		<input id="field" type="text" />
+	</p>
+	
+	<table>
+		<tr>
+			<td>keydown</td>
+			<td id="down"></td>
+		</tr>
+		<tr>
+			<td>keypress</td>
+			<td id="press"></td>
+		</tr>
+		<tr>
+			<td>keyup</td>
+			<td id="up"></td>
+		</tr>
+	</table>
+	
+	<script>
+		var field = document.getElementById('field'),
+			down = document.getElementById('down'),
+			press = document.getElementById('press'),
+			up = document.getElementById('up');
+	
+		document.addEventListener('keydown', function(e) {
+			down.innerHTML = e.keyCode;
+		});
+	
+		document.addEventListener('keypress', function(e) {
+			press.innerHTML = e.keyCode;
+		});
+	
+		document.addEventListener('keyup', function(e) {
+			up.innerHTML = e.keyCode;
+		});
+	</script>*/
+
+	
